195b651f8242f55f72c30b3e590db7463e1582e3
Dimosthenis Kalaitzis <ftv.voidblaze@gmail.com>
2020-08-17 23:04:06 +0300

    Switch to main instead of anonymous function

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
+import dotenv from 'dotenv'
+dotenv.config()
 import { Place } from './place'
 import yargs from 'yargs'
-;(async () => {
+
+let main = async () => {
   yargs.argv._[0]
     .split(',')
     .map((place) => place.trim())
     .map(async (place) => await Place.fromString(place))
-})()
+}
+
+main()
